a341857412620b2b33b1d924302bf3537529b62a
Giuseppe Ruggieri <giuseppe.ruggieri@GL05318M.local>
2016-01-24 23:01:55 +0000

    Fixed React examples

--- src/common/reactExamples/examples.js
+++ src/common/reactExamples/examples.js
@@ -1,23 +1,11 @@
-// var React = require('react');
-// var ReactDOM = require('react-dom');
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export function examples() {
+	var reactElementP = React.createElement('p', { className: 'p-el', key: 'p' }, 'hi p tag');
+	return reactElementP;
 }
 
 export function example1() {
-	var reactElementP = React.createElement('p', { className: 'p-el', key: 'p' }, 'hi p tag');
-	//ReactDOM.render(reactElementP, example1);
-
-}
-
-export function example2() {
-	//var container = React.createElement('section', null, [ reactElementH1, reactElementP ]);
-	//ReactDOM.render(container, example2);
-}
-
-export function example3() {
 	var TodoList = React.createClass({
 		render: function () {
 			var createItem = function (itemText, index) {
@@ -28,10 +16,10 @@ export function example3() {
 		}
 	});
 
-	//ReactDOM.render(<TodoList joe={ [ 'Joe', 'Bharathy', 'Simon', 'Murali', 'Sai', 'Leila', 'Emanuele', 'Chris' ] } />, example3);
+	return <TodoList joe={ [ 'Joe', 'Bharathy', 'Simon', 'Murali', 'Sai', 'Leila', 'Emanuele', 'Chris' ] } />;
 }
 
-export function example4() {
+export function example2() {
 	var Clock = React.createClass({
 		getInitialState: function () {
 			return { time: this._getTime() };
@@ -56,6 +44,10 @@ export function example4() {
 		}
 	});
 
-	//var tt = React.createElement(Clock, null);
+	return React.createElement(Clock, null);
 }
+
+export function example3() {}
+
+export function example4() {}
 
