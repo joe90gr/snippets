a56005debb852ab36947d035946906cd81ce9b15
Giuseppe Ruggieri <Giuseppe.Ruggieri@Gamesys.co.uk>
2016-04-04 15:29:50 +0100

    Adds Sass preprocessor and refactored CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,18 @@ var gulp = require('gulp');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var sass = require('gulp-sass');
 var buffer = require('vinyl-buffer');
 var sourcemaps = require('gulp-sourcemaps');
 var Server = require('karma').Server;
+
+gulp.task('sass', function () {
+	gulp.src('./public/styles/scss/style.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./public/styles'));
+});
 
 gulp.task('test', function (done) {
 	new Server({
@@ -22,3 +31,6 @@ gulp.task('browserify', function () {
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('./public/js'));
 });
+
+gulp.task('default', [ 'test', 'sass', 'browserify' ]);
+gulp.task('build', [ 'sass', 'browserify' ]);
