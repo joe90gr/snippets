33443cfc8e1b45e65bae532d598a2ea0ac5a06ac
Giuseppe Ruggieri <Giuseppe.Ruggieri@Gamesys.co.uk>
2016-04-25 09:38:00 +0100

    deletes store when unmounted.

--- src/mixins/use.js
+++ src/mixins/use.js
@@ -14,6 +14,7 @@ export function use(storeName, onChangeHandler) {
 
 		componentWillUnmount: function () {
 			this[storeName].removeChangeListener(this[onChangeHandler]);
+			delete this[storeName];
 		}
 	};
 }
