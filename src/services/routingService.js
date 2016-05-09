151716873679c5d6396ab1cf70b6326673dd39e0
Giuseppe Ruggieri <Giuseppe.Ruggieri@Gamesys.co.uk>
2016-05-09 10:17:25 +0100

    Cleaned up routingService

--- src/services/routingService.js
+++ src/services/routingService.js
@@ -2,9 +2,7 @@ import director from 'director';
 import routes from 'configuration/routes';
 import NavigateAction from 'actions/NavigateAction';
 
-let bindEvents = {
-	routes: routes
-};
+let bindEvents = { routes: routes };
 
 for (let route in routes) {
 	if (routes.hasOwnProperty(route)) {
@@ -14,12 +12,10 @@ for (let route in routes) {
 	}
 }
 
-delete bindEvents.routes;
-
 function CallbackObj(_route) {
-	this.fn = () => {
-		NavigateAction.navigateTo(_route);
-	};
+	this.fn = () => NavigateAction.navigateTo(_route);
 }
+
+delete bindEvents.routes;
 
 export default new director.Router(bindEvents).configure({ html5history: true });
