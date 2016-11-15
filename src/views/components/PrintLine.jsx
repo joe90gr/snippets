e64eea88167ba4e61fdca6250e186584f2733d62
Giuseppe Ruggieri <Giuseppe.Ruggieri@Gamesys.co.uk>
2016-11-15 17:29:52 +0000

    Convert PrintLine to ES6 classes

--- src/views/components/PrintLine.jsx
+++ src/views/components/PrintLine.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-export default React.createClass({
-	displayName: 'PrintLine',
-
-	propTypes: {
-		items: React.PropTypes.array
-	},
-
-	render: function () {
+class PrintLine extends React.Component {
+	render() {
 		var createItem = function (line, index) {
 			return React.createElement('p', { className: 'line' + index, key: 'pl' + (index + 1) }, line);
 		};
 
 		return <div>{this.props.items.map(createItem)}</div>;
 	}
-});
+}
+
+PrintLine.displayName = 'PrintLine';
+
+PrintLine.propTypes = {
+	items: React.PropTypes.array
+};
+
+export default PrintLine;
