6a60ee6b5fdaf5c157106e68bf6b47aeef107f03
Giuseppe Ruggieri <Giuseppe.Ruggieri@Gamesys.co.uk>
2016-11-15 18:08:34 +0000

    Convert PrintConsole to ES6 classes

--- src/views/components/PrintConsole.jsx
+++ src/views/components/PrintConsole.jsx
@@ -2,31 +2,36 @@ import React from 'react';
 import events from 'utils/events';
 import PrintLine from './PrintLine';
 
-export default React.createClass({
-	displayName: 'PrintConsole',
+class PrintConsole extends React.Component {
+	constructor(props) {
+		super(props);
 
-	propTypes: {
-		context: React.PropTypes.string
-	},
+		this.state = { items: [] };
+	}
 
-	getInitialState: function () {
-		return { items: [] };
-	},
-
-	componentDidMount: function () {
+	componentDidMount() {
 		events.on(this.props.context, this._onChange);
-	},
+	}
 
-	componentWillUnmount: function () {
+	componentWillUnmount() {
 		events.off(this.props.context, this._onChange);
-	},
+	}
 
-	render: function () {
+	render() {
 		return <PrintLine items={this.state.items}/>;
-	},
+	}
 
-	_onChange: function (result) {
+	_onChange(result) {
 		var nextItems = this.state.items.concat([ result ]);
+
 		this.setState({ items: nextItems });
 	}
-});
+}
+
+PrintConsole.displayName = 'PrintConsole';
+
+PrintConsole.propTypes = {
+	context: React.PropTypes.string
+};
+
+export default PrintConsole;
