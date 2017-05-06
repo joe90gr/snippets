473aebfb00ac3e39c6d043f3399e3b907a3720be
Giuseppe Ruggieri <Giuseppe.Ruggieri@Gamesys.co.uk>
2017-05-06 19:29:48 +0100

    Cleaned up unessesary file fragmentation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
 import {} from 'contribution/serviceRegistration.server';
 import {} from 'contribution/storeRegistration';
-import app from './middleware';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import { express, router } from './utils/express-utils';
+import createServer from './createServer';
 
-export default app();
+var app = express();
+createServer(app);
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(session({
+	name: 'JOEID',
+	cookie: {
+		path: '/',
+		httpsOnly: true,
+		maxAge: null,
+		secure: true
+	},
+	'saveUninitialized': false,
+	secret: '1234567890QWERTY',
+	resave: false
+}));
+app.use('/', router);
+app.use(router['404']);
+app.use(router['500']);
--- src/createServer.js
+++ src/createServer.js
@@ -1,63 +1,64 @@
 import fs from'fs';
-import app from '../lib/app';
 import _debug from 'debug';
 import http from 'http';
 import https from 'https';
 
-const debug = _debug('gulp-browserify-react:server');
-const https_options = {
-	key: fs.readFileSync('./certificates/14961049-localhost.key'),
-	cert: fs.readFileSync('./certificates/14961049-localhost.cert')
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+export default function (app) {
+	const debug = _debug('gulp-browserify-react:server');
+	const https_options = {
+		key: fs.readFileSync('./certificates/14961049-localhost.key'),
+		cert: fs.readFileSync('./certificates/14961049-localhost.cert')
+	};
+	const port = normalizePort(process.env.PORT || '3000');
+	app.set('port', port);
 
-const server = http.createServer(app);
-const httpsServer = https.createServer(https_options, app);
+	const server = http.createServer(app);
+	const httpsServer = https.createServer(https_options, app);
 
-server.listen(port);
-httpsServer.listen(3443, 'localhost');
-server.on('error', onError);
-server.on('listening', onListening);
+	server.listen(port);
+	httpsServer.listen(3443, 'localhost');
+	server.on('error', onError);
+	server.on('listening', onListening);
 
-function normalizePort(val) {
-	const port = parseInt(val, 10);
+	function normalizePort(val) {
+		const port = parseInt(val, 10);
 
-	if (isNaN(port)) {
-		return val;
+		if (isNaN(port)) {
+			return val;
+		}
+
+		if (port >= 0) {
+			return port;
+		}
+
+		return false;
 	}
 
-	if (port >= 0) {
-		return port;
-	}
+	function onError(error) {
+		const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-	return false;
-}
-
-function onError(error) {
-	const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-	if (error.syscall !== 'listen') {
-		throw error;
-	}
-
-	switch (error.code) {
-		case 'EACCES':
-			console.error(bind + ' requires elevated privileges');
-			process.exit(1);
-			break;
-		case 'EADDRINUSE':
-			console.error(bind + ' is already in use');
-			process.exit(1);
-			break;
-		default:
+		if (error.syscall !== 'listen') {
 			throw error;
+		}
+
+		switch (error.code) {
+			case 'EACCES':
+				console.error(bind + ' requires elevated privileges');
+				process.exit(1);
+				break;
+			case 'EADDRINUSE':
+				console.error(bind + ' is already in use');
+				process.exit(1);
+				break;
+			default:
+				throw error;
+		}
 	}
-}
 
-function onListening() {
-	const addr = server.address();
-	const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+	function onListening() {
+		const addr = server.address();
+		const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
 
-	debug('Listening on ' + bind);
+		debug('Listening on ' + bind);
+	}
 }
