de727ccfc760900a6899938ac48df088970c5c83
Giuseppe Ruggieri <Giuseppe.Ruggieri@Gamesys.co.uk>
2017-05-24 11:44:00 +0100

    Adds optimisation for unnecessary re-renders for LoginForm

--- src/common/components/LoginForm.jsx
+++ src/common/components/LoginForm.jsx
@@ -5,53 +5,63 @@ import userAction from 'platform/actions/UserAction';
 
 import AccessControl from './AccessControl';
 
-function LoginForm({ user: { name }, errors }) {
-	let username, password;
+class LoginForm extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-	function _onSubmitLogin(e) {
+	_onSubmitLogin(e) {
 		e.preventDefault();
 
 		userAction.authenticateUser({
 			body: {
-				userName: username.value,
-				password: password.value
+				userName: this._username.value,
+				password: this._password.value
 			}
 		});
 	}
 
-	function _onSubmitLogout(e) {
+	_onSubmitLogout(e) {
 		e.preventDefault();
 
 		userAction.invalidateUser();
 	}
 
-	return (
-		<div>
-			<AccessControl loggedIn={ false }>
-				<div>
-					<h1>Hello Guest</h1>
-					<form method="get" action="/login" onSubmit={ (e) => _onSubmitLogin(e) }>
-						<input type="text" ref={(value) => {
-							username = value;
-						}} defaultValue="joe" />
-						<input type="text" ref={(value) => {
-							password = value;
-						}} defaultValue="1234" />
-						<button type="submit">Login</button>
-					</form>
-				</div>
-			</AccessControl>
-			<AccessControl loggedIn>
-				<div>
-					<h1>Hello { name }</h1>
-					<form method="get" action="/logout" onSubmit={ (e) => _onSubmitLogout(e) }>
-						<button type="submit">Logout</button>
-					</form>
-				</div>
-			</AccessControl>
-			<p>{ errors }</p>
-		</div>
-	);
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.user !== this.props.user;
+	}
+
+	render() {
+		const { user: { name }, errors } = this.props;
+
+		return (
+			<div>
+				<AccessControl loggedIn={ false }>
+					<div>
+						<h1>Hello Guest</h1>
+						<form method="get" action="/login" onSubmit={ (e) => this._onSubmitLogin(e) }>
+							<input type="text" ref={(value) => {
+								this._username = value;
+							}} defaultValue="joe" />
+							<input type="text" ref={(value) => {
+								this._password = value;
+							}} defaultValue="1234" />
+							<button type="submit">Login</button>
+						</form>
+					</div>
+				</AccessControl>
+				<AccessControl loggedIn>
+					<div>
+						<h1>Hello { name }</h1>
+						<form method="get" action="/logout" onSubmit={ (e) => this._onSubmitLogout(e) }>
+							<button type="submit">Logout</button>
+						</form>
+					</div>
+				</AccessControl>
+				<p>{ errors }</p>
+			</div>
+		);
+	}
 }
 
 LoginForm.displayName ='LoginForm';
